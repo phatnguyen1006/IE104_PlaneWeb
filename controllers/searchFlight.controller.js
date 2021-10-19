ea59c0a02bad3cb8737ce7fe62f2fe95fd036102
KhoiLe <Khoilecontact@gmail.com>
2021-10-19 22:04:05 +0700

    Khoile - 19/10 - Fix date formatter

--- controllers/searchFlight.controller.js
+++ controllers/searchFlight.controller.js
@@ -14,6 +14,10 @@ module.exports.postTicket = async (req, res) => {
         ChuyenBay : journey
     };
 
+    var date = new Date();
+    var timeArr = await time.getOne();
+    var day = timeArr.minTimeBookedTicket; 
+
     var flights = await takeListFlight.findFlightSchedules(flight);
 
     for(let i=0 ; i < flights.length; i++) {
@@ -44,11 +48,6 @@ module.exports.postTicket = async (req, res) => {
                 flightsReturn[i].DuocMua = 0;
             } 
         }
-
-
-        var date = new Date();
-        var timeArr = await time.getOne();
-        var day = timeArr.minTimeBookedTicket; 
     
         // Chuyến bay đi
         var arrayDate = [];
@@ -308,11 +307,7 @@ module.exports.postTicket = async (req, res) => {
 
     // Vé một chiều
     } else {
-        var date = new Date();
-        var timeArr = await time.getOne();
-        var day = timeArr.minTimeBookedTicket; 
-    
-        
+
         var arrayDate = [];
         var arrayDate1 = [];
         var arrayHour_depart = [];
@@ -419,8 +414,8 @@ module.exports.postTicket = async (req, res) => {
 
             
             //console.log("in ra date1", arrayDate1); 
-
-            arrayDate[0] = formatDate(arrayDate[0]);
+            
+            arrayDate[0] = formatDate(arrayDate[0]); 
             //console.log("in ra date", arrayDate[0]);
 
             res.render('searchFlight', {
@@ -451,114 +446,8 @@ function formatDate(date) {
     var day = date.getDate();
 
     console.log(`${month+1}/${day}/${year}`);
-    var newDate = `${month+1}/${day + 1}/${year}`;
+    var newDate = `${month+1}/${day}/${year}`;
 
     return new Date(newDate);
 }
 
-function formatFlightDate(flights) {
-    var arrayDate = [];
-    var arrayDate1 = [];
-    var arrayHour_depart = [];
-    var arrayHour_arrival = [];
-    var arrayHour_TGB = [];
-    var arrayMinute_depart = [];
-    var arrayMinute_arrival = [];
-    var arrayMinute_TGB = [];
-
-    for(let i=0 ; i < flights.length; i++) {
-        var ktra = false;
-        var addDate = false;
-        if(flights[i].GioKhoiHanh >= flights[i].GioDen ){
-            ktra = true;
-        }else{
-            ktra = false;
-        }
-        var TGB = flights[i].ThoiGianBay;
-        var minute_TGB = parseInt((TGB - parseInt(TGB))*60);
-
-        TGB = parseInt(flights[i].ThoiGianBay);
-
-        var myDate = new Date(flights[i].NgayGio);
-        arrayDate.push(myDate);
-
-        var myDate_1 = new Date(flights[i].NgayGio);
-        var hour_depart = flights[i].GioKhoiHanh;
-        var minute_depart = parseInt((hour_depart - parseInt(hour_depart))*60);
-
-        arrayMinute_depart.push(minute_depart);
-        hour_depart = parseInt(flights[i].GioKhoiHanh);
-        arrayHour_depart.push(hour_depart);
-
-        var hour_arrival = flights[i].GioDen;
-        var minute_arrival = parseInt((hour_arrival - parseInt(hour_arrival))*60);
-
-        hour_arrival = parseInt(flights[i].GioDen);
-
-        if(flights[i].SanBayTG.leng > 0){
-            for(var j=0;j < flights[i].SanBayTG.length;j++ ){
-                minute_arrival = parseInt(minute_arrival) + parseInt(flights[i].SanBayTG[j].TGDung);
-                minute_TGB += parseInt(flights[i].SanBayTG[j].TGDung);
-            }
-        }
-
-
-        if(minute_arrival >= 60){
-            minute_arrival = parseInt(minute_arrival) - 60;
-            hour_arrival += 1;
-            
-            if(hour_arrival == 24){
-                hour_arrival = 0;
-                addDate = true;
-            }
-        }
-        
-        if(minute_TGB >= 60){
-            minute_arrival = parseInt(minute_arrival) - 60;
-            TGB += 1;
-        }
-        
-        arrayHour_TGB.push(TGB);
-        arrayMinute_TGB.push(minute_TGB);
-        arrayHour_arrival.push(hour_arrival);
-        arrayMinute_arrival.push(minute_arrival);
-        
-        if(ktra === true){
-            if(addDate === true){
-                Date.prototype.addDays = function(){
-                    var date = new Date(flights[i].NgayGio);
-                    date.setUTCDate(date.getDate() + 2);
-                    return date;
-                }
-            }else{
-                Date.prototype.addDays = function(){
-                    var date = new Date(flights[i].NgayGio);
-                    date.setUTCDate(date.getDate() + 1);
-                    return date;
-                }
-            }
-            myDate_1 = myDate_1.addDays();
-            arrayDate1.push(myDate_1);
-        }
-        else{
-            if(addDate === true){
-                Date.prototype.addDays = function(){
-                    var date = new Date(flights[i].NgayGio);
-                    date.setUTCDate(date.getDate() + 1);
-                    return date;
-                    
-                }
-            }else{
-                
-                Date.prototype.addDays = function(){
-                    var date = new Date(flights[i].NgayGio);
-                    return date;
-                }
-            }
-            myDate_1 = myDate_1.addDays();
-            arrayDate1.push(myDate_1);
-        }
-    }
-
-    return myDate_1;
-}
