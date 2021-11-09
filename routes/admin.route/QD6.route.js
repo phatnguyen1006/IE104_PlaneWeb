bfe5c62b80daf70234154e93f53df66b1ca594b1
P <19521998@gm.uit.edu.vn>
2021-11-09 21:25:51 +0700

    merge khoile setup

--- routes/admin.route/QD6.route.js
+++ routes/admin.route/QD6.route.js
@@ -1,47 +1,51 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-//controller 
-const controller = require('../../controllers/admin.controller/QD6.controller');
+//controller
+const controller = require("../../controllers/admin.controller/QD6.controller");
 
 //validate
-const validate = require('../../validate/QD6.validate');
+const validate = require("../../validate/QD6.validate");
 
-//san bay 
-router.get('/', controller.getQD6);
+//san bay
+router.get("/", controller.getQD6);
 
-router.post('/', controller.postAddAirport);
+router.post("/", controller.postAddAirport);
 
-router.post('/deleteAirport', controller.postDeleteAirport);
+router.post("/deleteAirport", controller.postDeleteAirport);
 
-router.post('/updateAirport', controller.postUpdateAirport);
+router.post("/updateAirport", controller.postUpdateAirport);
 
-router.get('/defaultDay', controller.getDefaultDay);
+router.get("/defaultDay", controller.getDefaultDay);
 
-router.post('/defaultDay', controller.postDefaultDay);
+router.post("/defaultDay", controller.postDefaultDay);
 
 //router.post('/changeSLGH', controller.postSLHG);
 
-router.get('/ticketClass', controller.getTicketClass);
+router.get("/ticketClass", controller.getTicketClass);
 
-router.post('/addHV', validate.addHV, controller.postAddHV);
+router.post("/addHV", validate.addHV, controller.postAddHV);
 
-router.post('/changeHV', controller.postChangeHV);
+router.post("/changeHV", controller.postChangeHV);
 
-router.post('/deleteHV', controller.postDeleteHV);
+router.post("/deleteHV", controller.postDeleteHV);
 
-router.get('/flightManagement', controller.getFlightManagement);
+router.get("/flightManagement", controller.getFlightManagement);
 
-router.get('/flightSchedule', controller.getFLightSchedule);
+router.get("/flightSchedule", controller.getFLightSchedule);
 
-router.post('/flightSchedule', validate.addFlightScheduleValidate, controller.postAddFlightSchedule);
+router.post(
+  "/flightSchedule",
+  validate.addFlightScheduleValidate,
+  controller.postAddFlightSchedule
+);
 
-router.get('updateFlightSchedule', controller.getUpdateFlightSchedule);
+router.get("/updateFlightSchedule", controller.getUpdateFlightSchedule);
 
-router.post('/updateFlightSchedule', controller.postUpdateFlightSchedule);
+router.post("/updateFlightSchedule", controller.postUpdateFlightSchedule);
 
-router.post('/reset', controller.postReset);
+router.post("/reset", controller.postReset);
 
-router.post('/deleteFlightScheudle', controller.deleteFlightSchedule);
+router.post("/deleteFlightScheudle", controller.deleteFlightSchedule);
 
 module.exports = router;
