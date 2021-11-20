8fcbd0444e4b9c2a4bf0b07d7f5509a6dc9ff742
P <19521998@gm.uit.edu.vn>
2021-11-20 10:37:12 +0700

    hahahahaha

--- controllers/viewTicket.controller.js
+++ controllers/viewTicket.controller.js
@@ -1,132 +1,132 @@
-const findBookedTicket = require('../services/flightTickets.service/phieuDC.service');
-const findBoughtTicket = require('../services/flightTickets.service/veMB.service');
-const findFlight = require('../services/flightSchedules.service');
-const getTime = require('../services/QD6.service');
-const HangVeService = require('../services/flightTickets.service/hangVe.service');
+const findBookedTicket = require("../services/flightTickets.service/phieuDC.service");
+const findBoughtTicket = require("../services/flightTickets.service/veMB.service");
+const findFlight = require("../services/flightSchedules.service");
+const getTime = require("../services/QD6.service");
+const HangVeService = require("../services/flightTickets.service/hangVe.service");
 
-//Function: find ticket of user or admin and transfer data to FE to show 
+//Function: find ticket of user or admin and transfer data to FE to show
 //Input: user info from locals or cookie
 //Output:  booked and bought tickets of user
 module.exports.findTicket = async (req, res) => {
-    //console.log(req.session.notify)
+  //console.log(req.session.notify)
 
-    const HangVe = await HangVeService.getAllTypeTicket();
-    var id=null;
+  const HangVe = await HangVeService.getAllTypeTicket();
+  var id = null;
 
-    if (req.signedCookies.userId) {
-        id = req.signedCookies.userId;
-    }
-    else if (req.signedCookies.adminId) {
-        id = req.signedCookies.adminId;
-    }
+  if (req.signedCookies.userId) {
+    id = req.signedCookies.userId;
+  } else if (req.signedCookies.adminId) {
+    id = req.signedCookies.adminId;
+  }
 
-    var bookedTicket = await findBookedTicket.findTicketsBooked(id);
-    var boughtTicket = await findBoughtTicket.findTicketsBought(id);
-    
-    const timeDB = await getTime.getOne();
-    const cancelTime = timeDB.cancelTimeBookTicket;
-    //console.log(cancelTime);
+  var bookedTicket = await findBookedTicket.findTicketsBooked(id);
+  var boughtTicket = await findBoughtTicket.findTicketsBought(id);
 
-    //add GiaVeMua and TrangThai to bookedTicket array of objects
-    var today = new Date(); 
-    for (let i=0 ; i<bookedTicket.length ; i++) {
-        const flight = await findFlight.getOneScheduleFlightByMaCB(bookedTicket[i].MaCB);
-        //add TrangThai 
-        if (flight.NgayGio-today >= (cancelTime*86400000)) {
-            bookedTicket[i].TrangThai=1;
-        }
-        else {
-            bookedTicket[i].TrangThai=0;
-        }
+  const timeDB = await getTime.getOne();
+  const cancelTime = timeDB.cancelTimeBookTicket;
+  //console.log(cancelTime);
 
-        //add GiaVeMua
-        bookedTicket[i] = {...bookedTicket[i],GiaVeMua : flight.GiaVe, TenHangVe: getTenHangVe(HangVe, bookedTicket[i].HangVe)};
-        //console.log(flight);
+  //add GiaVeMua and TrangThai to bookedTicket array of objects
+  var today = new Date();
+  for (let i = 0; i < bookedTicket.length; i++) {
+    const flight = await findFlight.getOneScheduleFlightByMaCB(
+      bookedTicket[i].MaCB
+    );
+    //add TrangThai
+    if (flight.NgayGio - today >= cancelTime * 86400000) {
+      bookedTicket[i].TrangThai = 1;
+    } else {
+      bookedTicket[i].TrangThai = 0;
     }
 
-    for (let i=0 ; i<boughtTicket.length ; i++) {
-        boughtTicket[i] = {...boughtTicket[i], TenHangVe: getTenHangVe(HangVe, boughtTicket[i].HangVe)};
-    }
-    //console.log(cancelTime);
+    //add GiaVeMua
+    bookedTicket[i] = {
+      ...bookedTicket[i],
+      GiaVeMua: flight.GiaVe,
+      TenHangVe: getTenHangVe(HangVe, bookedTicket[i].HangVe),
+    };
+    //console.log(flight);
+  }
 
-    
-    if (req.session.notify) {
-        //console.log(req.session.notify);
-        var notify = req.session.notify;
-        delete req.session.notify;
-        
-        res.render('viewTicket', {
-            bookedTicket: bookedTicket,
-            boughtTicket: boughtTicket,
-            cancel: cancelTime,
-            HangVe: HangVe,
-            csrf: req.csrfToken(),
-            notify: notify
-        });
-    }
-    else {
-        res.render('viewTicket', {
-            bookedTicket: bookedTicket,
-            boughtTicket: boughtTicket,
-            cancel: cancelTime,
-            HangVe: HangVe,
-            csrf: req.csrfToken(),
-        });
-    }
-    
+  for (let i = 0; i < boughtTicket.length; i++) {
+    boughtTicket[i] = {
+      ...boughtTicket[i],
+      TenHangVe: getTenHangVe(HangVe, boughtTicket[i].HangVe),
+    };
+  }
+  //console.log(cancelTime);
 
-}
+  if (req.session.notify) {
+    //console.log(req.session.notify);
+    var notify = req.session.notify;
+    delete req.session.notify;
+
+    res.render("viewTicket", {
+      bookedTicket: bookedTicket,
+      boughtTicket: boughtTicket,
+      cancel: cancelTime,
+      HangVe: HangVe,
+      csrf: req.csrfToken(),
+      notify: notify,
+    });
+  } else {
+    res.render("viewTicket", {
+      bookedTicket: bookedTicket,
+      boughtTicket: boughtTicket,
+      cancel: cancelTime,
+      HangVe: HangVe,
+      csrf: req.csrfToken(),
+    });
+  }
+};
 
 //Function: change ticket info (only booked ticket)
-//Input: id of ticket 
+//Input: id of ticket
 //Output: result of update ticket info
 module.exports.postTicketChange = async (req, res) => {
-    var ticketID = req.body._id;
-    var info = {
-        HanhKhach: req.body.name,
-        CMND : req.body.personalId,
-        SDT : req.body.phone,
-    }
-    const change = await findBookedTicket.updateTicketBooked(ticketID, info);
-    if (!change) {
-        req.session.notify = "Thay đổi thông tin thất bại";
-        
-    } else {
-        req.session.notify = "Thay đổi thông tin thành công";
-    }
-    res.redirect('/viewTicket');
-}
+  var ticketID = req.body._id;
+  var info = {
+    HanhKhach: req.body.name,
+    CMND: req.body.personalId,
+    SDT: req.body.phone,
+  };
+  const change = await findBookedTicket.updateTicketBooked(ticketID, info);
+  if (!change) {
+    req.session.notify = "Thay đổi thông tin thất bại";
+  } else {
+    req.session.notify = "Thay đổi thông tin thành công";
+  }
+  res.redirect("/viewTicket");
+};
 
-
-//Function: paid to change booked from bought ticket 
+//Function: paid to change booked from bought ticket
 //Input: id ticket
 //Output: save ticket it to bought ticket and delete it in booked ticket
 module.exports.postChange = async (req, res) => {
-    const data = {
-        id: req.body._id,
-        GiaTien: req.body.price
-    };
+  const data = {
+    id: req.body._id,
+    GiaTien: req.body.price,
+  };
 
-    const changeTicket = await findBookedTicket.changeBookedToBoughtTicket(data);
+  console.log("aaaaaaaaaaa", req.body);
 
-    if (!changeTicket) {
-        req.session.notify = "Bạn đã chuyển vé đặt thành mua thất bại";
-        console.log("Change ticket bought failed");
-    }
-    else {
-        req.session.notify = "Bạn đã chuyển vé đặt thành mua thành công";
-        console.log("Change ticket to bought sucessfully");
-    }
+  const changeTicket = await findBookedTicket.changeBookedToBoughtTicket(data);
 
-    res.redirect('/viewTicket')
-}
+  if (!changeTicket) {
+    req.session.notify = "Bạn đã chuyển vé đặt thành mua thất bại";
+    console.log("Change ticket bought failed");
+  } else {
+    req.session.notify = "Bạn đã chuyển vé đặt thành mua thành công";
+    console.log("Change ticket to bought sucessfully");
+  }
 
+  res.redirect("/viewTicket");
+};
 
 function getTenHangVe(HangVe, maHangVe) {
-    
-    for(hangve of HangVe) {
-        if(hangve.maHangVe === maHangVe) return hangve.tenHangVe;
-    }
+  for (hangve of HangVe) {
+    if (hangve.maHangVe === maHangVe) return hangve.tenHangVe;
+  }
 
-    return "";
+  return "";
 }
